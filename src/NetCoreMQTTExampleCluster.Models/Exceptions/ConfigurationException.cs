a9ec248f9d43faaa8e1072db8e702f7c6c957439
SeppPenner <HansM2013@gmx.de>
2022-04-22 17:36:56 +0200

    Added missing comments to configuration exception.

--- src/NetCoreMQTTExampleCluster.Models/Exceptions/ConfigurationException.cs
+++ src/NetCoreMQTTExampleCluster.Models/Exceptions/ConfigurationException.cs
@@ -1,12 +1,32 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigurationException.cs" company="Hämmer Electronics">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// <summary>
+//   An exception to handle configuration errors.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace NetCoreMQTTExampleCluster.Models.Exceptions;
 
+/// <inheritdoc cref="Exception"/>
+/// <summary>
+/// An exception to handle configuration errors.
+/// </summary>
 public class ConfigurationException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+    /// </summary>
     public ConfigurationException()
     {
     }
 
-    public ConfigurationException(string name) : base(name)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigurationException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public ConfigurationException(string message) : base(message)
     {
     }
 }
